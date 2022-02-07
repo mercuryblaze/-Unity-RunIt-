4aa52319cf015987daf4ab976a65e3a62bf094cc
mercuryblaze <77534657+mercuryblaze@users.noreply.github.com>
2022-02-07 23:37:19 +0300

    Added switching camera logic

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,6 +12,8 @@ public class MainMenu : MonoBehaviour
     public Transform Camera;
     public Transform CharSwitchHolder;
     public float CameraSpeed;
+    public GameObject[] Chars;
+    public int CurrentCharacter;
 
     private Vector3 CamTargetPos;
 
@@ -37,5 +39,23 @@ public class MainMenu : MonoBehaviour
         SwitchingScreen.SetActive(true);
 
        CamTargetPos = Camera.position + new Vector3(0f, CharSwitchHolder.position.y, 0f);
+    }
+
+    public void MoveLeft()
+    {
+        if (CurrentCharacter > 0)
+        {
+            CamTargetPos += new Vector3(2f, 0f, 0f);
+            CurrentCharacter--;
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (CurrentCharacter < Chars.Length - 1)
+        {
+            CamTargetPos -= new Vector3(2f, 0f, 0f);
+            CurrentCharacter++;
+        }
     }
 }
